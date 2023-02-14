49869a4e9edf93c6568daaa362f1f09a34ed1ca7
Ysunil9 <sunil.pcs24@gmail.com>
2023-02-14 22:04:54 +0530

    Welcome

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace CabIvoiceGenerate
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, Welcome to Cab Invoice Generator!");
+        }
+    }
+}
