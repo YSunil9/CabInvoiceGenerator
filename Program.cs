9858c9399035653aa53b0e41299fceef6b9a987b
Ysunil9 <sunil.pcs24@gmail.com>
2023-02-15 23:20:32 +0530

    Fare Calculated

--- Program.cs
+++ Program.cs
@@ -5,6 +5,9 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, Welcome to Cab Invoice Generator!");
+            InvoiceGenerator obj = new InvoiceGenerator();
+            double fare1 = obj.CalculateFare(12, 40);
+            Console.WriteLine("Total Fare for your Journey is :" + fare1);
         }
     }
 }
